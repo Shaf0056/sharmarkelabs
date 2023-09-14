41f8050ac87f72bea8388c7d9734de0c07322fff
Shaf0056 <113194302+Shaf0056@users.noreply.github.com>
2023-09-14 17:15:25 -0400

    Finished commit

---
+++ app/src/main/java/com/example/sharmarkelabs/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sharmarkelabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sharmarkelabs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
