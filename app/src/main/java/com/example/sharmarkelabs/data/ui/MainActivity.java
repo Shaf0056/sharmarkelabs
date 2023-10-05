527ace8d627186dda4480a3875352b93ab780c29
Shaf0056 <113194302+Shaf0056@users.noreply.github.com>
2023-10-05 16:07:01 -0400

    Commited

--- app/src/main/java/com/example/sharmarkelabs/data/ui/MainActivity.java
+++ app/src/main/java/com/example/sharmarkelabs/data/ui/MainActivity.java
@@ -2,46 +2,82 @@ package com.example.sharmarkelabs.data.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sharmarkelabs.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
+    Button loginButton ;
+    EditText emailtext;
+    Intent nextPage ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        loginButton = findViewById(R.id.button);
+        emailtext = findViewById(R.id.editTextTextEmailAddress);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        emailtext.setText(emailAddress);
 
-        imgView = findViewById(R.id.flagview);
-        sw = findViewById(R.id.switch1);
-
-        sw.setOnCheckedChangeListener((btn, isChecked) -> {
-            if (isChecked) {
-                RotateAnimation rotate = new RotateAnimation(
-                        0, 360,
-                        Animation.RELATIVE_TO_SELF, 0.5f,
-                        Animation.RELATIVE_TO_SELF, 0.5f
-                );
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            } else {
-                imgView.clearAnimation();
-            }
-        });
+        loginButton.setOnClickListener( clk-> {
+            String EmailAddress = emailtext.getText().toString();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", EmailAddress);
+            editor.apply();
+            nextPage = new Intent(MainActivity.this,SecondActivity.class);
+            nextPage.putExtra("EmailAddress",emailtext.getText().toString());
+            startActivity( nextPage);
+        });}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "onStart() - The activity is now visible on screen");
     }
-}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "onResume() - The activity has focus and the user can interact with it");
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - The application no longer responds to user input");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity","In onStop() - The application is no longer visible.");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "onDestroy() - The activity is being destroyed");
+
+    }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/sharmarkelabs/data/ui/SecondActivity.java
@@ -0,0 +1,114 @@
+package com.example.sharmarkelabs.data.ui;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sharmarkelabs.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class SecondActivity extends AppCompatActivity {
+    Button CallNum;
+    Intent nextPage;
+    Button cam;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent fromPrevious = getIntent();
+        String EmailAddress = fromPrevious.getStringExtra("EmailAddress");
+
+        CallNum = findViewById(R.id.button3);
+        cam = findViewById(R.id.button4);
+        TextView textView = findViewById(R.id.textView);
+        Button changePic = findViewById(R.id.button4);
+        ImageView img = findViewById(R.id.imageView);
+        textView.setText("Welcome back " + EmailAddress);
+        EditText PhoneNum = findViewById(R.id.editTextPhone2);
+        CallNum.setOnClickListener(clk -> {
+            String number = PhoneNum.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + number));
+            startActivity(call);
+        });
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+
+            @Override
+
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode() == Activity.RESULT_OK) {
+
+                    Intent data = result.getData();
+                    Bitmap thumbnail = data.getParcelableExtra("data");
+                    img.setImageBitmap(thumbnail);
+
+                    FileOutputStream fOut = null;
+
+                    try {
+                        fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+
+                        thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                        fOut.flush();
+                        fOut.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+
+            }
+
+        });
+        File file = new File(getFilesDir(), "Picture.png");
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+            img.setImageBitmap(theImage);
+
+        }
+
+        cam.setOnClickListener(clk -> {
+            cameraResult.launch(cameraIntent);
+
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EditText phoneNumber = findViewById(R.id.editTextPhone2);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String newPhoneNumber = phoneNumber.getText().toString();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("PhoneNumber", newPhoneNumber);
+        editor.apply();
+    }
+}
